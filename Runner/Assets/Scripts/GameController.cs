efef11481806e93a6bf6a9dfff8481ec5eb6a0e3
Adam <lambatti@wp.pl>
2019-11-30 23:10:02 +0100

    Dodano skrypt obsługujący grę

---
+++ Runner/Assets/Scripts/GameController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public int score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        score = (int)Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
