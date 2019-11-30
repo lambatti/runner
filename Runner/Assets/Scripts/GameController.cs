d36a12468e8b9a91fc66d1a360aeb9635d749c4d
Adam <lambatti@wp.pl>
2019-12-01 00:50:54 +0100

    Dodano zliczanie punktów

--- Runner/Assets/Scripts/GameController.cs
+++ Runner/Assets/Scripts/GameController.cs
@@ -1,20 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameController : MonoBehaviour
 {
-    public int score;
+    public float score;
+
+    public TextMeshProUGUI scoreText;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        score = 0;
     }
 
     void FixedUpdate()
     {
-        score = (int)Time.time;
+        score += Time.deltaTime*10;
+        scoreText.text = "Score: " + (int)score;
     }
 
     // Update is called once per frame
