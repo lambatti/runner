5c95e12c76ef69e9bc51660c270449888dda7df1
Adam <lambatti@wp.pl>
2019-12-05 22:41:35 +0100

    Zmiana algorytmu liczenia punktów

--- Runner/Assets/Scripts/GameController.cs
+++ Runner/Assets/Scripts/GameController.cs
@@ -3,29 +3,33 @@ using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.SceneManagement;
+using System.Diagnostics;
 
 public class GameController : MonoBehaviour
 {
-    public float score;
+    public long score;
 
     public TextMeshProUGUI scoreText;
 
+    private Stopwatch sw;
 
     // Start is called before the first frame update
     void Start()
     {
         score = 0;
+        sw = new Stopwatch();
+        sw.Start();
     }
 
     void FixedUpdate()
     {
-        score += Time.deltaTime*10;
-        scoreText.text = "Score: " + (int)score;
+        score = sw.ElapsedMilliseconds / 100;
+        scoreText.text = "Score: " + score;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
